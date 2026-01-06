e69cbb659ab3d2ac2ce2d281f435f46d16f4d090
Andres <18741308+a-gracia@users.noreply.github.com>
2026-01-06 04:32:45 -0500

    fix: cable, queue, cache

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,25 +7,3 @@ bin/rails assets:precompile
 bin/rails assets:clean
 
 bin/rails db:migrate
-
-bin/rails runner "
-conn = ActiveRecord::Base.connection
-
-# Solid Queue
-unless conn.table_exists?('solid_queue_jobs')
-  puts 'Loading queue schema...'
-  system('bin/rails db:queue:schema:load') or exit(1)
-end
-
-# Solid Cache
-unless conn.table_exists?('solid_cache_entries')
-  puts 'Loading cache schema...'
-  system('bin/rails db:cache:schema:load') or exit(1)
-end
-
-# Solid Cable
-unless conn.table_exists?('solid_cable_messages')
-  puts 'Loading cable schema...'
-  system('bin/rails db:cable:schema:load') or exit(1)
-end
-"
